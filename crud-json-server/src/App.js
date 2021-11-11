f6a71f6f6b8eed416c181f4021ee74ad4c9dce15
Fan <52841919+fantastic-y@users.noreply.github.com>
2021-11-10 19:25:46 -0800

    created the app and getlists function

---
+++ crud-json-server/src/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        loading: false,
+        alldata: [],
+        singledata: {
+          title: "",
+          author: ""
+        }
+      };
+    }
+
+    getLists = () => {
+      fetch("http://localhost:5000/posts")
+      .then (res => res.json())
+      .then (result =>
+        this.setState({
+          loading: false,
+          alldata: result
+        })
+      )
+      .catch(console.log);
+    }
+
+    render() {
+      const listTable = this.state.loading ? () : ();
+      return (
+        <>
+          <div className="container">
+            <span className="title-bar">
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={this.getLists}
+              >
+                Get Lists
+              </button>
+            </span>
+          </div>
+        </>
+      )
+    }
+  }
+
+export default App;
